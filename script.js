2a71ef91c998865839f5690b518cafb92ea85508
Shane Cooper <pleadingsanity1@gmail.com>
2025-07-27 00:07:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const feed = document.getElementById("scroll-feed");
+  feed.innerHTML = "<p>This is your future scroll feed. Let's rise from madness.</p>";
+});
